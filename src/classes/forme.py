99282846a88955cc92bef44681b6f54f77c2b203
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-02-24 13:37:45 +0100

    test

---
+++ src/classes/forme.py
@@ -0,0 +1,20 @@
+class Forme():
+
+    def __init__(self, pos1, pos2, form):
+        self.pos1 = pos1
+        self.pos2 = pos2
+        self.form = form
+
+    def gauche(self):
+        if (pos1[0] > 0):
+            self.pos1[0] = self.pos1[0] - 1
+            self.pos2[0] = self.pos2[0] - 1
+
+    def droite(self):
+        if (pos1[0] < 10):
+            self.pos1[0] = self.pos1[0] + 1
+            self.pos2[0] = self.pos2[0] + 1
+
+    def bas(self):
+        self.pos1[1] = self.pos1[1] + 1
+        self.pos2[1] = self.pos2[1] + 1
--- src/serveur/main.py
+++ src/serveur/main.py
@@ -1,3 +1,15 @@
+import sys
+import os
+import time
+import imp
+forme = imp.load_source('Forme', '../classes/forme.py')
+joueur = imp.load_source('Joueur', '../classes/joueur.py')
+carte = imp.load_source('Carte', '../classes/carte.py')
+import random
+from PodSixNet.Channel import Channel
+from PodSixNet.Server import Server
+import time,sys
+
 MAP =  {0 :[0,0,0,0,0,0,0,0,0,0],
         1 :[0,0,0,0,0,0,0,0,0,0],
         2 :[0,0,0,0,0,0,0,0,0,0],
@@ -25,73 +37,137 @@ Block_Ligne = {0 : { 0 : [1],
                      1 : [1],
                      2 : [1],
                      3 : [1]},
+
                1 : { 0 : [1,1,1,1]},
+
                2 : { 0 : [1],
                      1 : [1],
                      2 : [1],
                      3 : [1]},
+
                3 : {0 : [1,1,1,1]}}
 
-Block_Tee =   {0 : { 0 : [0,2,0],
-                     1 : [2,2,2]},
-               1 : { 0 : [0,2],
-                     1 : [2,2],
-                     2 : [0,2]},
-               2 : { 0 : [2,2,2],
-                     1 : [0,2,0]},
-               3 : { 0 : [2,0],
-                     1 : [2,2],
-                     2 : [2,0]}}
+Block_Tee =   {0 : { 0 : [-1, 2,-1],
+                     1 : [ 2, 2, 2]},
 
-Block_S1 =    {0 : {0 : [3,3,0],
-                    1 : [0,3,3]},
-               1 : {0 : [0,3],
-                    1 : [3,3],
-                    2 : [3,0]},
-               2 : {0 : [3,3,0],
-                    1 : [0,3,3]},
-               3 : {0 : [0,3],
-                    1 : [3,3],
-                    2 : [3,0]}}
+               1 : { 0 : [-1,2],
+                     1 : [ 2, 2],
+                     2 : [-1, 2]},
 
-Block_S2 =      {0 : {0 : [0,4,4],
-                      1 : [4,4,0]},
-                 1 : {0 : [4,0],
-                      1 : [4,4],
-                      2 : [0,4]},
-                 2 : {0 : [0,4,4],
-                      1 : [4,4,0]},
-                 3 : {0 : [4,0],
-                      1 : [4,4],
-                      2 : [0,4]}}
+               2 : { 0 : [ 2, 2, 2],
+                     1 : [-1, 2,-1]},
+
+               3 : { 0 : [ 2,-1],
+                     1 : [ 2, 2],
+                     2 : [ 2,-1]}}
+
+Block_S1 =    {0 : {0 : [ 3, 3,-1],
+                    1 : [-1, 3, 3]},
+
+               1 : {0 : [-1, 3],
+                    1 : [ 3, 3],
+                    2 : [ 3,-1]},
+
+               2 : {0 : [ 3, 3,-1],
+                    1 : [-1, 3, 3]},
+
+               3 : {0 : [-1, 3],
+                    1 : [ 3, 3],
+                    2 : [ 3,-1]}}
+
+Block_S2 =      {0 : {0 : [-1, 4, 4],
+                      1 : [ 4, 4,-1]},
+
+                 1 : {0 : [ 4,-1],
+                      1 : [ 4, 4],
+                      2 : [-1, 4]},
+
+                 2 : {0 : [-1, 4, 4],
+                      1 : [ 4, 4,-1]},
+
+                 3 : {0 : [ 4,-1],
+                      1 : [ 4, 4],
+                      2 : [-1, 4]}}
 
 Block_Cube =   {0 : {0 : [5,5],
                      1 : [5,5]},
+
                 1 : {0 : [5,5],
                      1 : [5,5]},
+
                 2 : {0 : [5,5],
                      1 : [5,5]},
+
                 3 : {0 : [5,5],
                      1 : [5,5]}}
 
-Block_L1=      { 0 : {0 : [6,6],
-                      1 : [0,6],
-                      2 : [0,6]},
-                 1 : {0 : [6,6,6],
-                      1 : [6,0,0]},
-                 2 : {0 : [0,6],
-                      1 : [0,6],
-                      2 : [6,6]},
-                 3 : {0 : [0,0,6],
-                      1 : [6,6,6]}}
+Block_L1=      { 0 : {0 : [ 6,6],
+                      1 : [-1,6],
+                      2 : [-1,6]},
 
-Block_L2=      { 0 : {0 : [7,7],
-                      1 : [7,0],
-                      2 : [7,0]},
-                 1 : {0 : [7,0,0],
-                      1 : [7,7,7]},
-                 2 : {0 : [0,7],
-                      1 : [0,7],
-                      2 : [7,7]},
-                 3 : {0 : [7,7,7],
-                      1 : [0,0,7]}}
+                 1 : {0 : [ 6, 6, 6],
+                      1 : [ 6,-1,-1]},
+
+                 2 : {0 : [-1,6],
+                      1 : [-1,6],
+                      2 : [ 6,6]},
+
+                 3 : {0 : [-1,-1,6],
+                      1 : [ 6, 6,6]}}
+
+Block_L2=      { 0 : {0 : [ 7, 7],
+                      1 : [ 7,-1],
+                      2 : [ 7,-1]},
+
+                 1 : {0 : [ 7,-1,-1],
+                      1 : [ 7, 7, 7]},
+
+                 2 : {0 : [-1, 7],
+                      1 : [-1, 7],
+                      2 : [ 7, 7]},
+
+                 3 : {0 : [ 7, 7, 7],
+                      1 : [-1,-1, 7]}}
+class ClientChannel(Channel):
+
+    def __init__(self, *args, **kwargs):
+        Channel.__init__(self, *args, **kwargs)
+        self.joueur = joueur.Joueur
+
+    def Close(self):
+        self._server.del_client(self)
+
+    def Network_keys(self,data):
+        touches = data['keystrokes']
+        if(touches[K_LEFT]):
+            pass
+        if(touches[K_RIGHT]):
+            pass
+        if(touches[K_UP]):
+            pass
+        if(touches[K_DOWN]):
+            pass
+
+    def update():
+        sefl.send({"""todo"""});
+
+class MyServer(Server):
+
+    channelClass = ClientChannel
+
+    def __init__(self, *args, **kwargs):
+        Server.__init__(self, *args, **kwargs)
+        self.clients = []
+        self.run = False
+        print('Server launched')
+
+    def Connected(self, channel, addr):
+        self.clients.append(channel)
+        channel.number = len(self.clients)
+        print('New connection: %d client(s) connected' % len(self.clients))
+        if len(self.clients) == 1:
+            self.run = True
+
+    def update_all(self):
+        for client in self.clients:
+            client.update()
