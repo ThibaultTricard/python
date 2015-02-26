ca2983c2762eba1b1668b8e1c71f4902fb71a5a1
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-02-26 20:23:46 +0100

    début du screen

--- perso/classes/jeu.py
+++ perso/classes/jeu.py
@@ -1,15 +1,27 @@
 import sys
 import time
 from classes import client
+import pygame
+from pygame.locals import *
+
+LARGEUR_FENETRE=int(800)
+HAUTEUR_FENETRE=int(600)
 
 class Jeu() :
     def __init__(self, ip, port, pseudo):
+        self.surface=pygame.display.set_mode((LARGEUR_FENETRE,HAUTEUR_FENETRE))
         self.ip=ip
         self.port=port
         self.pseudo=pseudo
 
+
     def lancer(self):
         monClient = client.Client(self.ip, self.port, self.pseudo)
         while True:
+            pygame.time.Clock().tick(60)
             monClient.Loop()
-            time.sleep(0.001)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit(0)
--- src/client/views/Accueil.py
+++ src/client/views/Accueil.py
@@ -18,8 +18,8 @@ class Accueil():
     def __init__(self):
         #AccueilController permet de gérer l'action qui suivra l'appui des touches
         self.accueilController=AccueilController(self)
-        
-    
+
+
     def afficherAccueil(self):
         self.surface=pygame.display.set_mode((LARGEUR_FENETRE,HAUTEUR_FENETRE))
         #chargement du background
@@ -28,69 +28,66 @@ class Accueil():
         self.creerBoutonNouvellePartie(CHEMIN_NOUVELLE_PARTIE)
         #chargement bouton quitter
         self.creerBoutonQuitter(CHEMIN_BOUTON_QUITTER)
-        
+
         #affichage des objets
         self.afficherObjet(self.background,self.boutonNouvellePartie,POSITION_NOUVELLE_PARTIE)
         self.afficherObjet(self.background,self.boutonQuitter,POSITION_QUITTER)
         self.afficherObjet(self.surface,self.background,(0,0))
-        
+
         #Booléen nécessaire pour arrêter la boucle loop()
         self.changementPage=False
-        
+
         #Mise à jour de l'affichage
         pygame.display.flip()
         self.loop()
-        
+
     def loop(self):
         while self.changementPage==False:
             pygame.time.Clock().tick(60)
-            
+
             #affichage des objets
             self.afficherObjet(self.surface,self.background,(0,0))
             self.afficherObjet(self.background,self.boutonNouvellePartie,POSITION_NOUVELLE_PARTIE)
             self.afficherObjet(self.background,self.boutonQuitter,POSITION_QUITTER)
-            
+
             #mise à jour de l'affichage
             pygame.display.flip()
-            
+
             #gestion des évènements
             for event in pygame.event.get():
                 if event.type==MOUSEBUTTONDOWN:
                     #le controller récupère les coordonnées de la souris au moment du click
                     self.accueilController.setMouseX(event.pos[0])
                     self.accueilController.setMouseY(event.pos[1])
-                    
+
                     #permet de faire une action en fonction du click
                     self.accueilController.controlerClick()
-                if event.type == QUIT:     
-                    sys.exit(0)        
-                           
+                if event.type == QUIT:
+                    sys.exit(0)
+
     def setBackground(self,backgroundImage):
         self.background=pygame.image.load(backgroundImage).convert()
-    
+
     def afficherObjet(self,surface,objet,(x,y)):
         return surface.blit(objet,(x,y))
-        
+
     def creerBoutonNouvellePartie(self,boutonJouer):
         self.boutonNouvellePartie=pygame.image.load(boutonJouer).convert_alpha()
-                    
+
     def creerBoutonQuitter(self,boutonQuitter):
         self.boutonQuitter=pygame.image.load(boutonQuitter).convert_alpha()
-        
+
     def getSurface(self):
         return self.surface
-    
+
     def getRectBoutonNouvellePartie(self):
         return self.afficherObjet(self.background,self.boutonNouvellePartie,POSITION_NOUVELLE_PARTIE)
-    
+
     def getRectBoutonQuitter(self):
         return self.afficherObjet(self.background,self.boutonQuitter,POSITION_QUITTER)
-    
+
     def getAccueilController(self):
         return self.accueilController
-    
+
     def desinitAccueil(self):
         self.changementPage=True
-    
-        
-    
