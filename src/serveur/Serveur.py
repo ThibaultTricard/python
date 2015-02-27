1c5712789de76416a439cf0f650d43359c07d9fa
limmilkki <totor-du-40@hotmail.fr>
2015-02-27 14:13:13 +0100

    key serveur

--- src/serveur/Serveur.py
+++ src/serveur/Serveur.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+from PodSixNet.Channel import Channel
 from PodSixNet.Server import Server
 from ClientChannel import ClientChannel
 
@@ -20,3 +21,23 @@ class MyServer(Server):
     def del_client(self,channel):
         print('client deconnecte')
         self.clients.remove(channel)
+
+class ClientChannel(Channel):
+
+    def __init__(self, *args, **kwargs):
+        Channel.__init__(self, *args, **kwargs)
+        self.current_forme=pygame.Sprite.RenderClear()
+
+    def Close(self):
+        self._server.del_client(self)
+
+    def Network_keys(self,data):
+        touches = data['keystrokes']
+        if(touches[K_LEFT]):
+            current_forme.gauche()
+        if(touches[K_RIGHT]):
+            current_forme.droite()
+        if(touches[K_DOWN]):
+            current_forme.bas()
+        if(touches[K_SPACE]):
+            current_forme.rotate()
