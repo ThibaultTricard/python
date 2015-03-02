381f449123db4b30527131fd39dafaa23385ca5c
limmilkki <totor-du-40@hotmail.fr>
2015-03-02 23:57:51 +0100

    Joueur0

--- perso/Serveur.py
+++ perso/Serveur.py
@@ -29,17 +29,17 @@ class MyServer(Server):
     def launch_game(self):
         # Init Pygame
         print("game launched")
+        nbFormeJ0=random.randint(0,6)
         nbFormeJ1=random.randint(0,6)
         nbFormeJ2=random.randint(0,6)
         nbFormeJ3=random.randint(0,6)
-        nbFormeJ4=random.randint(0,6)
         dict=dictForme.DictForme()
         formes=dict.getFormes()
         for client in self.clients:
+            client.Send({"action":"former","joueur":0,"forme":formes[nbFormeJ0]})
             client.Send({"action":"former","joueur":1,"forme":formes[nbFormeJ1]})
             client.Send({"action":"former","joueur":2,"forme":formes[nbFormeJ2]})
             client.Send({"action":"former","joueur":3,"forme":formes[nbFormeJ3]})
-            client.Send({"action":"former","joueur":4,"forme":formes[nbFormeJ4]})
         while True:
             time.sleep(0.01)
             self.Pump()
